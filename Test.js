880b9351841791c8c5f2899fd39e2e3547ca67dc
Akash52 <ac8572611@gmail.com>
2021-07-13 21:29:12 +0530

    Create Student Schema & Insert Record

--- Test.js
+++ Test.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+//Database Connection
 const connect = () => {
   return mongoose.connect('mongodb://localhost:27017/whatever', {
     useUnifiedTopology: true,
@@ -7,4 +8,19 @@ const connect = () => {
     useNewUrlParser: true,
   })
 }
+
+//Here We Create our First Schema
+
+const studentSchema = new mongoose.Schema({
+  firstname: String,
+})
+
+//Here We Create Our Model
+const Student = mongoose.model('student', studentSchema)
+
 connect()
+  .then(async (connection) => {
+    const student = await Student.create({ firstname: 'Sky' })
+    console.log(student)
+  })
+  .catch((e) => console.error(e))
